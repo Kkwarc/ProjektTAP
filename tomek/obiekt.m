a3c687dcbd28e65597574fa61835e4a5d1e10412
Kkwarc <kwarc02@gmail.com>
2024-04-06 14:30:47 +0200

    pid odsprzeganie + basic dmc

--- tomek/obiekt.m
+++ tomek/obiekt.m
@@ -9,10 +9,10 @@ function [F_C, V, VT, T, F, h, T_out] = obiekt(F_Cin, F_H, F_D, F_C, T_H, T_C, T
     F_temp = alpha * sqrt(h_temp);
     dVdt_2 = F_H(k-1) + F_C(k-1) + F_D(k-1) - F_temp;
     dVTdt_2 = F_H(k-1) * T_H(k-1) + F_C(k-1) * T_C(k-1) + F_D(k-1) * T_D(k-1) - F_temp * T_temp;
-    V(k) = V(k-1) + T_p * dVdt_2;
-    VT(k) = VT(k-1) + T_p * dVTdt_2;
-    T(k) = VT(k) / V(k);
-    F(k) = alpha * sqrt(h(k-1));
-    h(k) = nthroot(V(k-1) / C, 3); 
+    V(k) = real(V(k-1) + T_p * dVdt_2);
+    VT(k) = real(VT(k-1) + T_p * dVTdt_2);
+    T(k) = real(VT(k) / V(k));
+    F(k) = real(alpha * sqrt(h(k-1)));
+    h(k) = real(nthroot(V(k-1) / C, 3)); 
     T_out(k) = T(k - tau_steps);
 end
